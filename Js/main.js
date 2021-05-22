c197aba6de595171082468597b00700a8d7302b1
IldenH <84644748+IldenH@users.noreply.github.com>
2021-05-23 00:31:22 +0200

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -2,5 +2,10 @@ var money = 0
 
 function moremoney() {
     money += 1
+};
+
+function updategui() {
     document.getElementById("Hello, World!").textContent = "Hello, World! " + money
+    
+    setTimeout(updategui, 0.1*1000)
 };
